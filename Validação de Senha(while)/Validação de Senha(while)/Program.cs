89f622229e49aa405b6200d09f1b251cd709df89
Raphael Simonetti <raphasimonettti@gmail,com>
2023-05-22 18:29:56 -0300

    Validação de Senha

---
+++ Validação de Senha(while)/Validação de Senha(while)/Program.cs
@@ -0,0 +1,25 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        string senha;
+        int i = 0;
+
+        Console.WriteLine("Para continuar, digite sua senha:");
+        senha = Console.ReadLine();
+
+        while (senha != "a6b5c4")
+        {
+            Console.WriteLine("Senha inválida");
+            i = i + 1;
+            Console.WriteLine("Para continuar, digite sua senha");
+            senha = Console.ReadLine();
+
+        }
+
+        Console.WriteLine("Seja bem-vindo(a) á área do cliente");
+
+    }
+};
