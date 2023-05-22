8098553005c0f7e6d0e5a53c36db747854006098
Raphael Simonetti <raphasimonettti@gmail,com>
2023-05-22 18:29:25 -0300

    Condição IF

---
+++ Trabalhando com IF/Trabalhando com IF/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        double x1, x2;
+        double res;
+
+        Console.WriteLine("Digite o valor de X1");
+        x1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o valor de X2");
+        x2 = Convert.ToDouble(Console.ReadLine());
+
+        if (x1 > x2)
+        {
+            res = (x1 - x2) / x1;
+        }
+        else
+        {
+            res = (x2 - x1) / x2;
+        }
+
+        Console.WriteLine($"O Resultado da Divisão é{res}");
+    }
+};
