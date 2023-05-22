14e20f43831f575927993f3bb76a5ae6817b0b11
Raphael Simonetti <raphasimonettti@gmail,com>
2023-05-22 18:26:38 -0300

    Kata de declarações de tipos de dados

---
+++ Exibição de Variáveis/Exibição de Variáveis/Program.cs
@@ -0,0 +1,12 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a, b;
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("a * b", a * b);
+
+    }
+};
