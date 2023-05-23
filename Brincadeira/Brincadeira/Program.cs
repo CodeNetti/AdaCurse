b591590f1277a4ffa922813d580fdd90c8a55a80
Raphael Simonetti <raphasimonettti@gmail,com>
2023-05-23 17:55:05 -0300

    Brincando com repetição dos dados

---
+++ Brincadeira/Brincadeira/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+     private static void Main()
+      {
+        double a, b, c;
+        a = 30;
+        b = 20;
+        c = a + b;
+
+        Console.WriteLine(c);
+
+        b = 10;
+
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        c = a + b;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        
+        
+
+
+      }
+
+}
