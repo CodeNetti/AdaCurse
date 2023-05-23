508cdb2914c31828f983f63015043169b7c5f6ca
Raphael Simonetti <raphasimonettti@gmail,com>
2023-05-23 17:53:54 -0300

    Algortimo para mostrar a criação de um nome de user

---
+++ MostrarLoginName/MostrarLoginName/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Diagnostics.SymbolStore;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string nome, dia, mes;
+
+        Console.WriteLine("Bem vindo ao App ADA");
+        Console.WriteLine("Digite seu  nome");
+        nome = Console.ReadLine();
+        Console.WriteLine("Digite o dia em que nasceu");
+        dia = Console.ReadLine();
+        Console.WriteLine("Digite seu mes de aniversario");
+        mes = Console.ReadLine();
+
+        Console.WriteLine($"Login:{dia + nome + mes}");
+        
+
+    }
+}
---
+++ TesteDeAlgoritimo/TesteDeAlgoritimo/Program.cs
@@ -0,0 +1,25 @@
+﻿
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        int t, m, c;
+        t = 1;
+        m = 5;
+        c = 10;
+
+        c = c * (t + 1);
+
+        
+        while (t < m) 
+        {
+
+
+            t = t + 1;
+        }
+        Console.WriteLine(c);
+
+
+    }
+}
